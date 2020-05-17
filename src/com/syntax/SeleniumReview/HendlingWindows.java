2eaca9983c499cc74f48ba6847dcc9ebf51e9431
ValeryTsybulka <valery171960@gmail.com>
2020-05-16 22:24:23 -0400

    Hendling Windows

---
+++ src/com/syntax/SeleniumReview/HendlingWindows.java
@@ -0,0 +1,35 @@
+package com.syntax.SeleniumReview;
+import java.util.Iterator;
+import java.util.Set;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HendlingWindows {
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.seleniumframework.com/Practiceform/");
+		// driver.manage().window().maximize();
+		WebElement newBrowserTab = driver.findElement(By.xpath("//button[text()='New Browser Tab']"));
+		newBrowserTab.click();
+		String parentWindow = driver.getWindowHandle();
+		Thread.sleep(2000);
+		driver.close();
+
+		Set<String> allWindows = driver.getWindowHandles();
+		Iterator<String> it = allWindows.iterator();
+		while (it.hasNext()) {
+			String childWindow = it.next();
+			if (!childWindow.equals(parentWindow)) {
+				driver.switchTo().window(childWindow);
+			}
+		}
+		WebElement header = driver.findElement(By.xpath("//strong[contains(text(),'Agile Testing ')]"));
+		String headerText = header.getText();
+		System.out.println(headerText);
+
+	}
+}
